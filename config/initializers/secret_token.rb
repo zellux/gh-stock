7ca8b650fc31f824c35d1a936d7c14fabf40719d
Yuanxuan Wang <zellux@gmail.com>
2012-06-07 10:35:38 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GhStock::Application.config.secret_token = '60f3e26153b974d1f495bc70f5e0bb67460167d5a855c7d5b14d35a923aecc8d8735105e40d48a3c33facef08e688480c68229836043e923f6c749eebd3fa3db'
