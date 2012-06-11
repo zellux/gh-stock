f8de45713c407feca96243666bfe9c08080adf21
Yuanxuan Wang <zellux@gmail.com>
2012-06-12 00:14:07 +0800

    Hide refresh button

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 GhStock::Application.routes.draw do
   root :to => 'home#index'
-  match '/refresh', :via => :post, :to => 'home#refresh'
+  match '/refresh', :via => [:post, :get], :to => 'home#refresh'
 end
